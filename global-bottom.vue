c03cd5ee1ce260487effa8f416dca4df7346fa0a
Philipp Wiesenbach <phiwi@users.noreply.github.com>
2026-05-28 12:00:25 +0200

    Fix slide page number visibility on dark slides

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="absolute bottom-3 right-5 text-sm opacity-75 select-none">
+  <footer class="absolute bottom-3 right-5 z-30 text-sm text-slate-100 opacity-90 select-none [text-shadow:0_1px_2px_rgba(0,0,0,0.8)]">
     {{ $nav.currentPage }} / {{ $nav.total }}
   </footer>
 </template>
